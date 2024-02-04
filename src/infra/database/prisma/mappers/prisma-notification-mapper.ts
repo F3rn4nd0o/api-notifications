5353af2ace8630e60354829aadd32539d7a6aeeb
F3rn4nd0o <fernandovasconcelosmelo@outlook.com>
2024-02-04 13:09:34 -0300

    feat(notifications): implementing prisma repository

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from '@prisma/client';
 import { Notification } from '@core/entities/notification';
+import { Content } from '@core/entities/content';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,19 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        canceledAt: raw.cancelAt,
+        createdAt: raw.createAt,
+        readAt: raw.readAt,
+      },
+      raw.id,
+    );
   }
 }
